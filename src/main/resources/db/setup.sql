b66b2b358b5b735f63df5217b5af417d8bc41795
Jeferson Almeida <jeferson.almeida@outlook.com>
2022-06-18 19:44:06 -0400

    Vert.x Reactive Mutiny

---
+++ src/main/resources/db/setup.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users
+(
+  id   SERIAL PRIMARY KEY,
+  name VARCHAR
+);
+
+insert into users(id, name)
+values (1, 'Helen');
+insert into users(id, name)
+values (2, 'Jeferson');
+insert into users(id, name)
+values (3, 'Maria');
